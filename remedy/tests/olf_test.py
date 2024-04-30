70952e6f445fbf75bf5bc9e1a276dafaf4d2334f
BiancaPedreschi <bianca.pedreschi@imtlucca.it>
2024-04-30 11:02:19 +0200

    small changes

--- remedy/tests/olf_test.py
+++ remedy/tests/olf_test.py
@@ -1,5 +1,7 @@
-from olfactometer_controller_B import olfactometer
 import os
+import sys
+sys.path.insert(0, os.getcwd())
+from remedy.olfactometer_controller import olfactometer
 import time
 
 # port = "/dev/cu.usbmodem20220051"
@@ -17,25 +19,25 @@ monitor.open(timeout=None)
 
 monitor.run()
 monitor.write('C M;;;;;;;')
-time.sleep(0.5)
+time.sleep(.5)
 monitor.write('E 1')
-time.sleep(0.5)
+time.sleep(.5)
 monitor.write('S 0')
-time.sleep(3)
+time.sleep(5)
 
 
 monitor.write('S 1')
-time.sleep(3)
+time.sleep(4)
 monitor.write('S 2')
-time.sleep(3)
+time.sleep(4)
 monitor.write('S 0')
-time.sleep(3)
-monitor.write('S 2')
-time.sleep(3)
-monitor.write('S 4')
-time.sleep(3)
-monitor.write('S 0')
-time.sleep(3)
+time.sleep(4)
+# monitor.write('S 2')
+# time.sleep(10)
+# monitor.write('S 4')
+# time.sleep(10)
+# monitor.write('S 0')
+# time.sleep(10)
 
 monitor.write('E 0')
 time.sleep(0.5)
